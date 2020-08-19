e4020f9895ec10a7e79cce5f638d4aaebfc74a86
cameron-poehner <cameronpoehner@gmail.com>
2020-08-19 14:21:52 -0700

    completed initial fully responsive website

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const mobileNav = document.querySelector('.nav-btn');
-const menu = document.querySelector('.navbar');
-mobileNav.addEventListener('click', function() {
-    menu.classList.add('open');
-});
+// ClassList - shows/gets classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggle class
+
+// Global Variables
+
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+
+// Event listeners
+
+navToggle.addEventListener('click', function() {
+    if (nav.classList.contains('hidden')) {
+        nav.classList.toggle('show-links');
+        nav.classList.toggle('hidden')
+    }
+})
